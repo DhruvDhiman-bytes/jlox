bf085e10b4a7578f92e090db8854887b60aa21fc
Dhruv Dhiman <dhruvdhiman905@gmail.com>
2026-09-04 16:27:23 +0530

    Added the comments to make it easier to understand

--- tree/scanner.java
+++ tree/scanner.java
@@ -10,19 +10,24 @@ import java.utils.List;
 
 public class Scanner {
     public static void main(String [] args) throws IOException {
-        if(args.length > 1) {
+
+        if(args.length > 1) { // if there is more information given to the command line
             System.out.println("Usage: jlox [script]");
             System.exit(64);
         }
-        else if (args.length == 1) {
+        else if (args.length == 1) { // when there is only the file name given to run the code file
             runFile(args[0]);
         }
-        else {
+        else { // when we need to directly talk with the interpreter, meaning write the code directly in the interpreter
             runPrompt();
         }
     }
+
+    // function that excepts the code file when given in the command
     private static void runFile(String path) throws IOException {
         byte[] bytes = Files.readAllBytes(Path.get(path));
         run(new String(bytes, Charset.defaultCharset()));
     }
+
+    // function for coding directly in the interpreter
 }
